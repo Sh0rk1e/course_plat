72f1e6286cfe259c1b9f519ea7eb299e2334d253
Qwer <qwertytrom09@gmail.com>
2026-09-20 14:10:37 +0200

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/course_plat/',
+  base: '/course_plat/', // Must match your GitHub repository name exactly
   build: {
     sourcemap: false,
   },
