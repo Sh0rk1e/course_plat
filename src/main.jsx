9cf7fadb7e8a6efb472133c192e6b0a1c9e434b5
Qwer <qwertytrom09@gmail.com>
2026-09-20 14:25:33 +0200

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,30 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './styles.css';
 
-const redirect = sessionStorage.redirect;
-if (redirect && redirect !== location.href) {
-  delete sessionStorage.redirect;
-  history.replaceState(null, '', new URL(redirect).pathname + new URL(redirect).search + new URL(redirect).hash);
-}
+// SPA GitHub Pages Redirection Handler:
+// Intercepts the query parameter (?p=) injected by 404.html and restores the clean browser URL state
+(function() {
+  var q = window.location.search;
+  if (q && q.indexOf('?p=') === 0) {
+    var parser = document.createElement('a');
+    parser.href = q.slice(3).replace(/~and~/g, '&');
+    
+    var path = parser.pathname;
+    var query = parser.search;
+    var hash = parser.hash;
+
+    window.history.replaceState(
+      null,
+      null,
+      path + query + hash
+    );
+  }
+})();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    {/* basename MUST match your GitHub repository name exactly */}
+    <BrowserRouter basename="/course_plat">
       <App />
     </BrowserRouter>
   </React.StrictMode>
